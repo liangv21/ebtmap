fe33a58f00279ac1cbcfbeed91bb2e3c2b5e6d97
Jonathan Yu <yu.jona@northeastern.edu>
2023-03-19 02:01:34 -0400

    Form formatting is done and a bit of coloring but can be corrected later

--- js/zipEnter.js
+++ js/zipEnter.js
@@ -3,16 +3,15 @@ const form = document.getElementById("zipcode-form");
 
 // Returns the zipcode if there is a valid zipcode, if return 
 function checkZip() {
-    var text;
     const textbox = document.getElementById("zipcode-textBox").value;
-    let zipcodeString = (String) textbox;
-    if (zipcodeString.length != 5) {
-        return -1; // -1 is a flag!
-    }
-    let zipcode = parseInt(zipcodeString);
+    let zipcodeString = "" + textbox;
+    if (zipcodeString.length != 5) { return -1; } // -1 is a flag! 
+    const zipcode = parseInt(zipcodeString);
+    if (isNAN(zipcode)) { return -1; } // -1 is a flag! 
     return zipcode;
 }
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
+    // getData(checkZip()); should call upon
 })
