3302aeb038b61d8b212cc6ab525ec8b8e6f5aa81
Jonathan Yu <yu.jona@northeastern.edu>
2023-03-19 00:15:41 -0400

    Added zipEnter.js and started to do form manipuation

--- js/zipEnter.js
+++ js/zipEnter.js
@@ -3,8 +3,9 @@ const form = document.getElementById("zipcode-form");
 
 // Returns the zipcode if there is a valid zipcode, if return 
 function checkZip() {
-    let textbox = document.getElementById("zipcode-textBox");
-    let zipcodeString = (String) textbox.value;
+    var text;
+    const textbox = document.getElementById("zipcode-textBox").value;
+    let zipcodeString = (String) textbox;
     if (zipcodeString.length != 5) {
         return -1; // -1 is a flag!
     }
