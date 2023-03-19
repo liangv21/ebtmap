36fa5c356da2b501606ac99539c2bf8eeef821d5
liangv21 <liang.va@northeastern.edu>
2023-03-19 10:22:43 -0400

    working link + about

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ localStorage.removeItem('objectToPass');
 
 getData("data/zipoutput.csv", "zipcode", zip);
 
-let mapData = null
+let mapData = null;
 let map = null;
 
 function getData(filename, type, zip) {
@@ -17,6 +17,28 @@ function getData(filename, type, zip) {
 	}})
 };
 
+let stateData = null;
+function readStateData(state) {
+	d3.csv("data/snap-ebt-resources.csv").then((data) => {
+		stateData = data;
+		getStateData(state, data)
+	})
+};
+
+function getStateData(state, stateData) {
+	stateData.forEach(function(point) {
+		if (point["state"] == state) {
+			document.getElementById("state-name").innerHTML = state.toUpperCase();
+			document.getElementById("snap-link").href = point["snap_web"];
+			document.getElementById("snap-schedule").href = point["snap_sched"];
+			document.getElementById("snap-offices").href = point["snap_office"];
+			document.getElementById("ebt-link").href = point["ebt_web"];
+			document.getElementById("snap-num").innerHTML = "SNAP Phone: " + point["snap_phone"];
+			document.getElementById("ebt-num").innerHTML = "EBT Phone: " + point["ebt_phone"]
+		}
+	})
+};
+
 function createMap(mapData, zip) {
 	mapData.forEach(function(point){
 		if (point["zip"] == zip){ 
@@ -27,6 +49,11 @@ function createMap(mapData, zip) {
 			L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
 				attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 			}).addTo(map);
+
+			// get snap office/ebt data from snap-ebt-resources.csv
+			state = point["state_name"];
+			// pass state into function that pulls the data from csv file
+			readStateData(state);
 		};
 	})
 
@@ -55,7 +82,7 @@ function plotPoints(plotData, zip) {
 							city + ", " + state + " " + zip_code;
 			let link = "<a href = \"https://www.google.com/search?q=" + name + " " + address1 + " " + address2 +
 			city + ", " + state + " " + zip_code + " \">" + popUpText + "</a>";
-			marker.bindPopup(popUpText);
+			marker.bindPopup(link);
 		};
 	})
 };
